ca963405f8a075f8fcadecc3df7ddb53602686b4
Noemi Radvan <radvanno@gmail.com>
2021-06-28 23:05:21 +0200

    second commit

--- script.js
+++ script.js
@@ -13,21 +13,50 @@ const additionButtonElement = document.querySelector("footer button");
 //Entry list
 const entryListElement = document.querySelector("section ul");
 
-console.log(balanceElement);
-console.log(valueInputElement);
-console.log(selectElement);
-console.log(additionButtonElement);
-console.log(entryListElement);
-
 function addEntry(amount, operation) {
   const listEntry = document.createElement("li");
+  listEntry.classList.add(operation);
 
   const listEntryValue = document.createElement("strong");
-  listEntryValue.innerText = amount;
+  listEntryValue.innerText = amount + "$";
+
+  const listEntryDescription = document.createElement("em");
+  listEntryDescription.innerText = "Description";
+
+  const listEntryOperator = document.createElement("span");
+  if (operation === "income") {
+    listEntryOperator.innerText = "+";
+  } else if (operation === "expense") {
+    listEntryOperator.innerText = "-";
+  }
+
+  listEntry.appendChild(listEntryDescription);
+
+  listEntry.appendChild(listEntryOperator);
 
   listEntry.appendChild(listEntryValue);
 
   entryListElement.appendChild(listEntry);
+}
+
+function updateBalance() {
+  let total = 0;
+  for (let item of entryListElement.children) {
+    console.log(item);
+    const valueElement = item.querySelector("strong");
+    const operationElement = item.querySelector("span");
+
+    const value = parseInt(valueElement.innerText);
+    const operation = operationElement.innerText;
+
+    if (operation === "+") {
+      total = total + value;
+    } else if (operation === "-") {
+      total = total - value;
+    }
+  }
+
+  balanceElement.innerText = total + "$";
 }
 
 additionButtonElement.onclick = function () {
@@ -36,4 +65,8 @@ additionButtonElement.onclick = function () {
   const operation = selectElement.value;
 
   addEntry(amount, operation);
+
+  valueInputElement.value = "";
+
+  updateBalance();
 };
